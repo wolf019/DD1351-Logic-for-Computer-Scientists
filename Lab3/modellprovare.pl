14e62628e278a97d0d842d4d0823e9b9d8de9af6
Tom Axberg <57027915+wolf019@users.noreply.github.com>
2020-12-14 18:33:35 +0100

    Update modellprovare.pl

--- Lab3/modellprovare.pl
+++ Lab3/modellprovare.pl
@@ -9,7 +9,7 @@
 % Computation Tree Logic: https://en.wikipedia.org/wiki/Computation_tree_logic
 %
 % check(V, L, S, U, F)
-% V - The transitions in form of adjacency lists (Verticies)
+% V - The Ls in form of adjacency lists (Verticies)
 % L - The labeling
 % S - Current state
 % U - Currently recorded states
@@ -17,7 +17,7 @@
 %
 % Tom Axberg
 % taxberg@kth.se
-% 2020-12-09
+% 2020-12-14
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -27,18 +27,6 @@ verify(Input) :-
         read(V), read(L), read(S), read(F), 
         seen, 
         check(V, L, S, [], F), !.
-
-%% Literals
-
-% p
-check(_, L, S, [], X) :- 
-        member([S, Ls], L),
-        member(X, Ls).
-
-% neg p
-check(_, L, S, [], neg(X)) :-
-        member([S, Ls], L),
-        \+member(X, Ls).
 
 % And
 check(V, L, S, [], and(F,G)) :- 
@@ -50,15 +38,20 @@ check(V, L, S, [], or(F,G)) :-
         check(V, L, S, [], F);
         check(V, L, S, [], G).
 
+% Imp
+check(V, L, S, [], imp(F,G)) :- 
+        \+check(V, L, S, [], F);
+        check(V, L, S, [], G).
+
 % AX
 check(V, L, S, [], ax(F)) :-
-        member([S, Transitions], V),
-        check_all(V, L, Transitions, [], F).
+        member([S, Ls], V),
+        check_all_states(V, L, Ls, [], F).
 
 % EX
 check(V, L, S, [], ex(F)) :-
-        member([S, Transitions], V),
-        check_exist(V, L, Transitions, [], F).
+        member([S, Ls], V),
+        check_exist_state(V, L, Ls, [], F).
 
 % AG1, S is in U
 check(_, _, S, U, ag(_)) :-
@@ -68,8 +61,8 @@ check(_, _, S, U, ag(_)) :-
 check(V, L, S, U, ag(F)) :-
         \+ member(S, U),
         check(V, L, S, [], F),
-        member([S, Transitions], V),
-        check_all(V, L, Transitions, [S|U], ag(F)).
+        member([S, Ls], V),
+        check_all_states(V, L, Ls, [S|U], ag(F)).
 
 % EG1
 check(_, _, S, U, eg(_)) :-
@@ -79,8 +72,8 @@ check(_, _, S, U, eg(_)) :-
 check(V, L, S, U, eg(F)) :- 
         \+ member(S, U),
         check(V, L, S, [], F),
-        member([S, Transitions], V),
-        check_exist(V, L, Transitions, [S|U], eg(F)).
+        member([S, Ls], V),
+        check_exist_state(V, L, Ls, [S|U], eg(F)).
 
 % % EF1
 check(V, L, S, U, ef(F)) :- 
@@ -90,9 +83,8 @@ check(V, L, S, U, ef(F)) :-
 % EF2
 check(V, L, S, U, ef(F)) :- 
         \+ member(S, U),
-        member([S, NextStates], V),
-        delete(NextStates, S, Transitions),
-        check_exist(V, L, Transitions, [S|U], ef(F)).
+        member([S, Ls], V),
+        check_exist_state(V, L, Ls, [S|U], ef(F)).
 
 % AF1
 check(V, L, S, U, af(F)) :-
@@ -102,33 +94,45 @@ check(V, L, S, U, af(F)) :-
 % AF2
 check(V, L, S, U, af(F)) :- 
         \+ member(S, U),
-        member([S, Transitions], V),
-        check_all(V, L, Transitions, [S|U], af(F)).
+        member([S, Ls], V),
+        check_all_states(V, L, Ls, [S|U], af(F)).
 
-%% check_all calls check witch all states in in first argument.
+%% Literals
+% p
+check(_, L, S, [], X) :- 
+        member([S, Ls], L),
+        member(X, Ls).
+
+% neg p
+check(_, L, S, [], neg(X)) :-
+        member([S, Ls], L),
+        \+member(X, Ls).
+
+
+%% check_all_states calls check witch all states in in first argument.
 % Fact
-check_all(_, _, [], _, _).
+check_all_states(_, _, [], _, _).
 
 % Handles cases when U is not empty.
-check_all(V, L, [H|T], U, X) :-
+check_all_states(V, L, [H|T], U, X) :-
         check(V, L, H, U, X),
-        check_all(V, L, T, [H|U], X).
+        check_all_states(V, L, T, [H|U], X).
 
 % Handles cases when U is empty.
-check_all(V, L, [H|T], [], X) :-
+check_all_states(V, L, [H|T], [], X) :-
         check(V, L, H, [], X),
-        check_all(V, L, T, [], X).
+        check_all_states(V, L, T, [], X).
 
-%% check_exist gives true if one call to check gives true.
+%% check_exist_state gives true if one call to check gives true.
 % Fact
-check_exist(_, _, [], _, _) :- fail.
+check_exist_state(_, _, [], _, _) :- fail.
 
 % Handles cases when U is not empty.
-check_exist(V, L, [H|T], U, X) :-
+check_exist_state(V, L, [H|T], U, X) :-
         check(V, L, H, U, X);
-        check_exist(V, L, T, [H|U], X).
+        check_exist_state(V, L, T, [H|U], X).
         
 % Handles cases when U is empty.
-check_exist(V, L, [H|T], [], X) :-
+check_exist_state(V, L, [H|T], [], X) :-
         check(V, L, H, [], X);
-        check_exist(V, L, T, [], X).
+        check_exist_state(V, L, T, [], X).
